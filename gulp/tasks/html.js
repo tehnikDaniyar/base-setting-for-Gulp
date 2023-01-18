807fcef822651cc269957dd188dceb3df2a8e567
Daniyar Bashirov <tehnikdaniyar@gmail.com>
2023-01-19 00:11:56 +0600

    added settings for gulp-replace

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -2,5 +2,6 @@ import fileinclude from "gulp-file-include";
 export const html = () => {
 	return app.gulp.src(app.path.src.html)
 		.pipe(fileinclude())
+		.pipe(app.plugins.replace(/\/base-setting-for-Gulp\/src\/img/gi, 'img'))
 		.pipe(app.gulp.dest(app.path.build.html));
 };
