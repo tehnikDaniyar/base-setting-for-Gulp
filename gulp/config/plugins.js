c639e8d3310c813c1ed7af875741f4d31a753608
Daniyar Bashirov <tehnikdaniyar@gmail.com>
2023-01-19 23:11:45 +0600

    added plugins plumber and notify

--- gulp/config/plugins.js
+++ gulp/config/plugins.js
@@ -1,5 +1,9 @@
-import replace from "gulp-replace"
+import replace from "gulp-replace";
+import plumber from "gulp-plumber";
+import notify from "gulp-notify";
 
 export const plugins = {
 	replace: replace,
+	plumber: plumber,
+	notify: notify,
 }
--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -5,6 +5,12 @@ import versionNumber from "gulp-version-number";
 
 export const html = () => {
 	return app.gulp.src(app.path.src.html)
+		.pipe(app.plugins.plumber(
+			app.plugins.notify.onError({
+				title: "HTML",
+				message: "Error: <%= error.message %>"
+			})
+		))
 		.pipe(fileinclude())
 		.pipe(app.plugins.replace(/\/base-setting-for-Gulp\/src\/img/gi, 'img'))
 		.pipe(webphtml())
