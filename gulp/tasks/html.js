11c0e419d403034baac7e77e0e30b0d999e02782
Daniyar Bashirov <tehnikdaniyar@gmail.com>
2023-01-19 22:59:01 +0600

    added two plugins for task 'html': webphtml and versionNumber

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,7 +1,26 @@
 import fileinclude from "gulp-file-include";
+import webphtml from "gulp-webp-html-nosvg";
+import versionNumber from "gulp-version-number";
+
+
 export const html = () => {
 	return app.gulp.src(app.path.src.html)
 		.pipe(fileinclude())
 		.pipe(app.plugins.replace(/\/base-setting-for-Gulp\/src\/img/gi, 'img'))
+		.pipe(webphtml())
+		.pipe(versionNumber({
+			'value': '%DT%',
+			'append': {
+				'key': '_v',
+				'cover': 0,
+				'to': [
+					'css',
+					'js',
+				]
+			},
+			'output': {
+				'file': 'gulp/version.json'
+			}
+		}))
 		.pipe(app.gulp.dest(app.path.build.html));
 };
